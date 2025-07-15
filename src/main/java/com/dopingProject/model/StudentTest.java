00f3b9a8f0585a148c48612919a08b2b36d4936e
“melhcn” <kosem21@itu.edu.tr>
2025-07-15 21:22:37 +0300

    index eklendi

--- src/main/java/com/dopingProject/model/StudentTest.java
+++ src/main/java/com/dopingProject/model/StudentTest.java
@@ -35,7 +35,7 @@ public class StudentTest {
     @Column(name = "selected_answer")
     private Map<Long, String> answers;
 
-    // --- GETTERS & SETTERS ---
+    
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
